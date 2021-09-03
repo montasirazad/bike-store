55f7ba58fbb385c074eca5a8db14e78fd01b9232
unknown <montasirazad@gmail.com>
2021-09-03 16:30:05 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import fakeData from '../src/Fake data/fakeData.json'
+import { useEffect, useState } from 'react';
+import Products from './Components/Products/Products';
+ 
 
 function App() {
+  const [bikes, setBikes] = useState([])
+
+  useEffect(() => {
+    setBikes(fakeData)
+    console.log(fakeData)
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <img style={{height:'300px'}} src=" https://i.pinimg.com/originals/14/d8/e5/14d8e57c7656356df80bced6f48f206b.png" alt="" />
+      <h1>Bike store</h1>
+
+      {
+        bikes.map(bike => <Products bike={bike} key={bike.id}> </Products>)
+      }
     </div>
   );
 }
---
+++ src/Components/Products/Products.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Products.css'
+
+const Products = (props) => {
+    const {img , model ,price ,overview,} = props.bike;
+     
+    return (
+        <div class='product'>
+           
+            <img style={{height:'400px'}} src={img} alt="" />
+            <h1> {model}  </h1>
+            <h2> Price: <small>{price}</small> BDT </h2>
+            <button>Buy Now ..!</button>
+            <p className='overview'>{overview}</p>
+             
+        </div>
+    );
+};
+
+export default Products;
