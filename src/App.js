372504ea8001cb3a26fea0405df66a54af27997e
unknown <montasirazad@gmail.com>
2021-09-06 02:51:52 +0600

    Using target="_blank" without rel="noreferrer" is a security risk

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className='header' >
-      <a href="https://www.youtube.com/" target='_blank'><img  style={{ height: '200px' }} src=" https://i.pinimg.com/originals/14/d8/e5/14d8e57c7656356df80bced6f48f206b.png"  alt='' /></a>
+      <a href="https://www.youtube.com/" target='_blank' rel="noreferrer"><img  style={{ height: '200px' }} src=" https://i.pinimg.com/originals/14/d8/e5/14d8e57c7656356df80bced6f48f206b.png"  alt='' /></a>
 
       <div className='app'>
         <Shop></Shop>
