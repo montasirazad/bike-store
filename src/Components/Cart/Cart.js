33f50f87612ab22983a382b00a3bd469622b4573
unknown <montasirazad@gmail.com>
2021-09-05 16:29:17 +0600

    li problem

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,7 +2,10 @@ import React from 'react';
 import '../Cart/Cart.css'
 
 const Cart = (props) => {
+    
     const  cart  = props.cart
+     
+   
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
@@ -14,6 +17,9 @@ const Cart = (props) => {
             <h2>Order Summery</h2>
             <h3>added items :{cart.length}</h3>
             <h4>Total price: {total}  </h4>
+            <ul>
+                <li> </li>
+            </ul>
         </div>
     );
 };
