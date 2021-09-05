40c5e8ac3ee29183f925751f30bdc0a2b1e3bc5a
unknown <montasirazad@gmail.com>
2021-09-05 16:17:38 +0600

    edit in fake data

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,11 +2,18 @@ import React from 'react';
 import '../Cart/Cart.css'
 
 const Cart = (props) => {
-    const Cart = props.cart
+    const  cart  = props.cart
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price
+
+    }
     return (
         <div className='cart'>
-            <h2>this is cart</h2>
-            <h3>added items :{Cart.length}</h3>
+            <h2>Order Summery</h2>
+            <h3>added items :{cart.length}</h3>
+            <h4>Total price: {total}  </h4>
         </div>
     );
 };
